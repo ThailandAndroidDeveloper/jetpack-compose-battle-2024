4ddabe93cc26f98acb03c8cdae7a552bae29161f
Ju Tipatai Puthanukunkit <judrummercpe@gmail.com>
2024-02-15 01:36:36 +0700

    feat: trigger ci

--- common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/easy1/Easy1Screen.kt
+++ common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/easy1/Easy1Screen.kt
@@ -19,18 +19,7 @@ import com.github.thailandandroiddeveloper.common.ui.theme.AppTheme
 @Composable
 private fun Easy1Screen(uiState: UiState) {
     // TODO
-    Box(modifier = Modifier.fillMaxSize()) {
-        Text(text = "Easy 1")
-        Text(text = "Easy 1")
-        Text(text = "Easy 1")
-        Text(text = "Easy 1")
-        Text(text = "Easy 1")
-        Text(text = "Easy 1")
-        Text(text = "Easy 1")
-        Text(text = "Easy 1")
-        Text(text = "Easy 1")
-        Text(text = "Easy 1")
-        Text(text = "Easy 1")
+    Box(modifier = Modifier.fillMaxSize().background(Color.Green)) {
         Text(text = "Easy 1")
     }
 }
