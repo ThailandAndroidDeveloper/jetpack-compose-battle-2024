7e757cc92d7d2f6919cb12703cd4f01e72e33475
Nakarin K <belizwp@live.com>
2024-02-17 17:49:56 +0700

    feat: implement easy 3 (WIP)

--- common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/easy3/Easy3Screen.kt
+++ common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/easy3/Easy3Screen.kt
@@ -3,21 +3,94 @@ package com.github.thailandandroiddeveloper.common.ui.screen.easy3
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.*
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.github.thailandandroiddeveloper.common.R
 import com.github.thailandandroiddeveloper.common.ui.preview.Pixel7
 import com.github.thailandandroiddeveloper.common.ui.theme.AppTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun Easy3Screen(uiState: UiState) {
-    // TODO
-    Box(modifier = Modifier.fillMaxSize().background(Color.Green)) {
-        Text(text = "Easy 3")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { /*TODO*/ },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            painter = painterResource(id = uiState.backIcon),
+                            contentDescription = null,
+                        )
+                    }
+                },
+                actions = {
+                    TextButton(onClick = { /*TODO*/ }) {
+                        Text(text = uiState.done, style = MaterialTheme.typography.labelLarge)
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Transparent
+                ),
+            )
+        },
+        containerColor = MaterialTheme.colorScheme.primaryContainer,
+    ) { paddingValues ->
+        Content(
+            Modifier
+                .padding(paddingValues)
+                .fillMaxSize(),
+            uiState = uiState,
+        )
+    }
+}
+
+@Composable
+private fun Content(
+    modifier: Modifier = Modifier, uiState: UiState
+) {
+    Column(
+        modifier = modifier,
+    ) {
+        Text(
+            uiState.title,
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(horizontal = 48.dp)
+        )
+        Text(
+            uiState.description,
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(horizontal = 48.dp)
+        )
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .clip(RoundedCornerShape(32.dp))
+                .background(MaterialTheme.colorScheme.onPrimary),
+        ) {
+        }
     }
 }
 
