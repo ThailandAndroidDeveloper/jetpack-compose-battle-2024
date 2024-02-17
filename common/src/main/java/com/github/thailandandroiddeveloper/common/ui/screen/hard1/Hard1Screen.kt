b22e597c92c833c2b81312e04c5d19d384d5d92d
Abdulrohim 'Him' Sama <a.rohim.sama@gmail.com>
2024-02-17 16:51:40 +0700

    update code

--- common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/hard1/Hard1Screen.kt
+++ common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/hard1/Hard1Screen.kt
@@ -2,13 +2,42 @@ package com.github.thailandandroiddeveloper.common.ui.screen.hard1
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.*
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.github.thailandandroiddeveloper.common.R
 import com.github.thailandandroiddeveloper.common.ui.preview.Pixel7
 import com.github.thailandandroiddeveloper.common.ui.preview.PixelTablet
@@ -16,9 +45,181 @@ import com.github.thailandandroiddeveloper.common.ui.theme.AppTheme
 
 @Composable
 private fun Hard1Screen(uiState: UiState) {
-    // TODO
-    Box(modifier = Modifier.fillMaxSize().background(Color.Green)) {
-        Text(text = "Hard 1")
+    val configuration = LocalConfiguration.current
+    configuration.screenWidthDp
+    Scaffold(
+        containerColor = Color(0xff6750A4)
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            if (configuration.screenWidthDp > 500) {
+                BottomLeftMenu(uiState)
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 17.dp)
+                    .padding(start = 236.dp, end = 128.dp),
+                verticalArrangement = Arrangement.Center,
+            ) {
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(2),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    content = {
+                        items(
+                            items = uiState.cards,
+                            itemContent = {
+                                when (it) {
+                                    is Card.Post -> CardPost(it)
+                                    is Card.Reward -> CardReward(card = it)
+                                    is Card.Suggestion -> CardSuggestion(card = it)
+                                }
+                            },
+                        )
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun CardSuggestion(card: Card.Suggestion) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = card.text,
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(onClick = { /*TODO*/ }) {
+                Text(
+                    text = card.moreButton,
+                    style = MaterialTheme.typography.labelSmall,
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
+                Icon(
+                    painter = painterResource(id = card.moreIcon),
+                    contentDescription = null
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun CardReward(card: Card.Reward) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(id = card.icon),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+        Spacer(modifier = Modifier.Companion.width(8.dp))
+        Text(
+            text = card.text,
+            style = MaterialTheme.typography.bodySmall,
+            color = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    }
+}
+
+@Composable
+private fun CardPost(card: Card.Post) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                color = MaterialTheme.colorScheme.primaryContainer,
+                RoundedCornerShape(8.dp)
+            )
+            .clip(RoundedCornerShape(8.dp)),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Row(
+            modifier = Modifier,
+        ) {
+            Image(
+                modifier = Modifier
+                    .width(180.dp)
+                    .height(224.dp),
+                painter = painterResource(id = card.photo),
+                contentDescription = null,
+                contentScale = ContentScale.FillBounds
+            )
+            Text(
+                modifier = Modifier.padding(16.dp),
+                text = card.text,
+                maxLines = 8,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onPrimaryContainer
+            )
+        }
+    }
+}
+
+@Composable
+private fun BoxScope.BottomLeftMenu(uiState: UiState) {
+    Column(
+        modifier = Modifier.Companion
+            .align(Alignment.BottomStart)
+            .padding(start = 32.dp, bottom = 40.dp)
+            .background(
+                color = MaterialTheme.colorScheme.background,
+                shape = RoundedCornerShape(12.dp)
+            )
+            .width(200.dp)
+            .height(200.dp),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        uiState.primaryMenus.forEachIndexed { index, primaryMenu ->
+            primaryMenu.menus.forEachIndexed { index, item ->
+                val color = if (item.selected)
+                    MaterialTheme.colorScheme.primaryContainer
+                else
+                    Color.White
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 10.dp)
+                        .padding(horizontal = 8.dp)
+                        .background(
+                            color = color,
+                            shape = RoundedCornerShape(8.dp)
+                        ),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        painter = painterResource(id = item.icon),
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onPrimaryContainer
+                    )
+                    Text(
+                        text = item.text,
+                        style = MaterialTheme.typography.titleSmall,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+        }
     }
 }
 
