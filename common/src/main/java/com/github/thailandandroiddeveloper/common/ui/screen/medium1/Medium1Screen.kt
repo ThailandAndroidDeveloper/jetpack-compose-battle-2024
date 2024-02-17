aa8a9e76a0666cf4caaef0d05785b4eecda98664
Royle <roylekoonlert@gmail.com>
2024-02-17 17:56:52 +0700

    fix

--- common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/medium1/Medium1Screen.kt
+++ common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/medium1/Medium1Screen.kt
@@ -6,20 +6,71 @@ import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.github.thailandandroiddeveloper.common.R
 import com.github.thailandandroiddeveloper.common.ui.preview.Pixel7
 import com.github.thailandandroiddeveloper.common.ui.theme.AppTheme
 
 @Composable
 private fun Medium1Screen(uiState: UiState) {
-    // TODO
     Column {
+        Column(
+            modifier = Modifier
+                .background(
+                    MaterialTheme.colorScheme.tertiaryContainer
+                )
+        ) {
+            Text(
+                modifier = Modifier
+                    .padding(
+                        top = 34.dp,
+                        start = 32.dp,
+                        end = 32.dp,
+                    ),
+                text = uiState.title,
+                style = MaterialTheme.typography.headlineMedium,
+                color = MaterialTheme.colorScheme.onTertiaryContainer
+            )
+            Text(
+                modifier = Modifier
+                    .padding(
+                        top = 16.dp,
+                        start = 32.dp,
+                        end = 32.dp,
+                    ),
+                text = uiState.subtitle,
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.tertiary
+            )
 
+            OutlinedTextField(
+                modifier = Modifier
+                    .align(
+                        alignment = Alignment.CenterHorizontally
+                    )
+                    .padding(
+                        top = 16.dp
+                    )
+                    .width(241.dp),
+                value = "", onValueChange = {},
+                colors = TextFieldDefaults.colors(
+                    unfocusedContainerColor = MaterialTheme.colorScheme.onPrimary
+                )
+            )
+        }
     }
 }
 
